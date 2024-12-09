5b7b32de83ed238f14befae6af144e91dce4720f
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-12-09 23:03:31 +0530

    kanban board and column ui

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,8 +1,46 @@
+import { useState } from "react";
+import PlusIcon from "../icons/PlusIcon";
+import { Column, Id } from "../types";
+import ColumnContainer from "./ColumnContainer";
+
 const KanbanBoard = () => {
+  const [columns, setColumns] = useState<Column[]>([]);
+  console.log(columns);
+  const createNewColumn = () => {
+    const columnToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`,
+    };
+    setColumns([...columns, columnToAdd]);
+  };
+
+  const generateId = () => {
+    return Math.floor(Math.random() * 10001);
+  };
+
+  const deleteColumn = (id: Id) => {
+    const filteredColumns = columns.filter((col) => col.id !== id);
+    setColumns(filteredColumns);
+  };
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-      <div className="m-auto">
-        <button className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 hover:transition-all">
+      <div className="m-auto flex gap-2">
+        <div className="flex gap-4">
+          {columns.map((col) => (
+            <div>
+              <ColumnContainer
+                key={col.id}
+                column={col}
+                deleteColumn={deleteColumn}
+              />
+            </div>
+          ))}
+        </div>
+        <button
+          onClick={() => createNewColumn()}
+          className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 hover:transition-all flex gap-2"
+        >
+          <PlusIcon />
           Add Column
         </button>
       </div>
